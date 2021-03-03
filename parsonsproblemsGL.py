fe4b27cd907fd6f7651b72aa1ef3cf8d7672081d
squishing456 <79872126+squishing456@users.noreply.github.com>
2021-03-03 14:18:47 -0800

    Add files via upload

---
+++ parsonsproblemsGL.py
@@ -0,0 +1,16 @@
+
+myHelloWorld = ["hello world", "hello world", "hello world", "hello world", "hello world", "hello world", "hello world", "hello world", "hello world", "hello world"]
+for x in myHelloWorld:
+    print(x)
+
+
+mySlam = ["Gutfed", "Cerebral Incubation", "Syphilectomy")
+print[mySlam]
+
+x = input("Type a number")
+if x > 0:
+    print("That's a small number!")
+    else:
+        x = int(x)
+        print("That's a small number!")
+    
